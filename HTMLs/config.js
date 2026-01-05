aa51a7a42581f5feb76125c21b54edb03bfa8e26
dwaugh <dwaugh@example.com>
2026-01-04 22:02:58 -0400

    Injected Supabase credentials into config.js.

--- HTMLs/config.js
+++ HTMLs/config.js
@@ -3,6 +3,6 @@
 // Students will never see these or be prompted for them.
 
 const CONFIG = {
-    SB_URL: "", // Placeholder - Teacher to fill
-    SB_KEY: ""  // Placeholder - Teacher to fill
+    SB_URL: "https://zwujpkuguevnkcqrkhas.supabase.co", // Placeholder - Teacher to fill
+    SB_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3dWpwa3VndWV2bmtjcXJraGFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczODA0NDQsImV4cCI6MjA4Mjk1NjQ0NH0.H9tVz_expVsE7VLLLLEsnTMVsm5PQ09r7KlLe85MirA"  // Placeholder - Teacher to fill
 };
